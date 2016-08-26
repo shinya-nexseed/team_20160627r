0504e4cbe215a7d3bc92fecbe90839d8a9a73433
tennyuu921 <tmtenyu@gmail.com>
2016-08-26 16:38:28 +0900

    toppageを更新

--- resources/views/toppage.php
+++ resources/views/toppage.php
@@ -5,7 +5,39 @@
 </head>
 <body>
 
-	<p><?php echo $name; ?></p>
+<form>
+
+	<h3>どんな映画が見たい？</h3>
+	<select name="factor1">
+		<?php foreach ($factor1 as $key => $value) : ?>
+			<option value="<?php echo $value['factor_id']; ?>"><?php echo $value['factor']; ?></option>
+		<?php endforeach; ?>
+	</select>
+	<br>
+
+	<h3>どんな気分になりたい？</h3>
+	<h4>一人目</h4>
+	<select name="factor2">
+		<?php foreach ($factor2 as $key => $value) : ?>
+			<option value="<?php echo $value['factor_id']; ?>"><?php echo $value['factor']; ?></option>
+		<?php endforeach; ?>
+	</select>
+
+	<h4>二人目</h4>
+	<select name="factor2">
+		<?php foreach ($factor2 as $key => $value) : ?>
+			<option value="<?php echo $value['factor_id']; ?>"><?php echo $value['factor']; ?></option>
+		<?php endforeach; ?>
+	</select>
+
+	<br>
+	<br>
+
+	<input type="submit" value="送信"><input type="reset" value="リセット">
+
+</form>
+
+
 
 </body>
 </html>
