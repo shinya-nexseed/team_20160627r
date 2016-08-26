9a43e26d469c64e5bb0a7dfdc75b1ca568496399
tennyuu921 <tmtenyu@gmail.com>
2016-08-26 16:38:13 +0900

    [WIP] controller を追記

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,16 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Factor;
 use App\Http\Requests;
 
 class SearchController extends Controller
 {
     public function index(){
-    	return view('toppage', ['testvalue' => 'test']);
+
+	    $factor1 = Factor::where('type', '映画種類')->get();
+	    $factor2 = Factor::where('type', 'シチュエーション')->get();
+
+    	return view('toppage', ['factor1' => $factor1->toArray(),'factor2' => $factor2->toArray()]);
 
     }
 
-    public function show($id){
+    public function show(){
     	//Model からデータを取得、全件DUMPするコードを記載
     	return view('show', ['testvalue' => 'show']);
 
