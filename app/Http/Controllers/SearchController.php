8f4b7c3aa0f146c0227a7fc50c80ae8d2c9fbcd3
tennyuu921 <tmtenyu@gmail.com>
2016-08-26 20:03:20 +0900

    WIP

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -12,18 +12,22 @@ class SearchController extends Controller
 {
     public function index(){
 
+    	//検索条件を取得
 	    $factor1 = Factor::where('type', '映画種類')->get();
 	    $factor2 = Factor::where('type', 'シチュエーション')->get();
 
     	return view('toppage', ['factor1' => $factor1->toArray(),'factor2' => $factor2->toArray()]);
-
     }
 
-    public function show($id){
 
+    public function show($fact1,$fact2,$fact3){
 	    $movie = Movie::where('movie_id', $id)->get();
 
-    	return view('show', ['data' => $movie->toArray()]);
+	    //レコメンド
+	    //	1.factにマッチする条件を全件取得
+	    //	2.その中からランダムに１件表示
 
+
+    	return view('show', ['data' => $movie->toArray()]);
     }
 }
